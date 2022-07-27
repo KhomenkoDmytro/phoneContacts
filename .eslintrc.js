7363d94a509e52643641ad9de336b7c1a0ab4fd3
KhomenkoDmytro <khomenko081102@gmail.com>
2022-07-27 23:37:50 +0300

    Database connected

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      es2021: true,
-      node: true,
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:sonarjs/recommended",
-      "plugin:import/recommended",
-    ],
-    plugins: ["sonarjs", "import"],
-    parserOptions: {
-      ecmaVersion: "latest",
-    },
-    rules: {
-      "newline-per-chained-call": "off",
-      "prefer-while": "off",
-    },
-  };
-  
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:import/recommended",
+  ],
+  plugins: ["sonarjs", "import"],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "newline-per-chained-call": "off",
+    "prefer-while": "off",
+  },
+};
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const express = require("express");
-const app = express();
-const PORT = 3000
-app.get("/");
-app.listen(PORT, () => console.log("Server working"));
+const app=require('./app');
+const mongoose=require("mongoose");
+
+const PORT = 3000;
+const DB_URL='mongodb://DmytroKhomenko:1234565@cluster1-shard-00-00.tj5db.mongodb.net:27017,cluster1-shard-00-01.tj5db.mongodb.net:27017,cluster1-shard-00-02.tj5db.mongodb.net:27017/?ssl=true&replicaSet=atlas-pdhpw5-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+mongoose
+  .connect(DB_URL)
+  .then(() => {
+    console.log('Database connection successful');
+    app.listen(3000);
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  });
