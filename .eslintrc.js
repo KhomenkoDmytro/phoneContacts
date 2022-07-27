4ad278ec901424d4b07f408dcd21f6727d44e7e5
KhomenkoDmytro <khomenko081102@gmail.com>
2022-07-27 18:58:48 +0300

    Linter and hooks are tuned

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    env: {
+      commonjs: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:sonarjs/recommended",
+      "plugin:import/recommended",
+    ],
+    plugins: ["sonarjs", "import"],
+    parserOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {
+      "newline-per-chained-call": "off",
+      "prefer-while": "off",
+    },
+  };
+  
